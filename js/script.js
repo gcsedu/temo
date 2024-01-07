4296b8496ab4d88e7083871c889ea8ae7ac71786
gcsedu <143339303+gcsedu@users.noreply.github.com>
2024-01-07 15:40:03 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+    // Get the button element by its id
+    var githubButton = document.getElementById('githubButton');
+
+    // Add a click event listener to the button
+    githubButton.addEventListener('click', function() {
+        // Redirect to the GitHub website
+        window.location.href = 'https://github.com/amna113';
+    });
